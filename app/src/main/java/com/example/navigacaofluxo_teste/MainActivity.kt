723e78c452d8160bf4c9fe2cdaf577a1865366f2
25132675 <eduardo.edubatista008@gmail.com>
2026-03-16 17:00:32 -0300

    Troca de Telas Funcionando

---
+++ app/src/main/java/com/example/navigacaofluxo_teste/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.navigacaofluxo_teste
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.navigacaofluxo_teste.screens.LoginScreen
+import com.example.navigacaofluxo_teste.screens.MenuScreen
+import com.example.navigacaofluxo_teste.screens.PedidosScreen
+import com.example.navigacaofluxo_teste.screens.PerfilScreen
+import com.example.navigacaofluxo_teste.ui.theme.NavigacaoFluxo_TesteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigacaoFluxo_TesteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login"
+                    ){
+                        composable ( route = "login" ){
+                            LoginScreen(navController = navController)
+                        }
+                        composable ( route = "menu" ){
+                            MenuScreen(navController = navController)
+                        }
+                        composable ( route = "perfil/{nome}" ){
+                            val nome = it.arguments?.getString("nome")
+                            PerfilScreen(navController = navController, nome = nome!!)
+                        }
+                        composable (
+                            route = "pedidos?numeroPedido={numeroPedido}",
+                            arguments = listOf(
+                                navArgument("numeroPedido" ){
+                                    defaultValue = "Sem Pedidos"
+                                }
+                            )){
+                            val numeroPedido = it.arguments?.getString("numeroPedido")
+                            PedidosScreen(navController = navController, numeroPedido = numeroPedido!!)
+                        }
+
+                    }
+//                    LoginScreen(
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                    MenuScreen()
+//                    PerfilScreen()
+//                    PedidosScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/navigacaofluxo_teste/screens/MenuScreen.kt
@@ -0,0 +1,94 @@
+package com.example.navigacaofluxo_teste.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun MenuScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(32, 129, 217, 255))
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "MENU",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            color = Color.White
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Button(
+                onClick = { navController.navigate(route = "perfil/sheldon")},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier.fillMaxWidth()
+
+            ) {
+                Text(
+                    text = "PERFIL",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Blue
+                )
+            }
+
+
+            Button(
+                onClick = { navController.navigate(route = "pedidos?numeroPedido=1234") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "PEDIDOS",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Blue
+                )
+            }
+
+
+            Button(
+                onClick = { navController.navigate(route = "login") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "SAIR",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Blue
+                )
+            }
+        }
+
+
+    }
+}
